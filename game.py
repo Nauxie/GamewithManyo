c66ba87dd3b56423098f91642abbb08a3f821ce7
mnj <mgntprg@gmail.com>
2019-06-17 22:44:39 -0700

    borardpyagaemcreation

--- game.py
+++ game.py
@@ -17,5 +17,30 @@ def print_tiles(board):
     for tile in board.board:
         print( (tile.x, tile.y) )
 
-board = Board(2, 2)
+
+
+board = Board(9, 5)
 print_tiles(board)
+
+width = 800
+height = 600
+white = (255, 255, 255)
+
+pygame.init()
+gameDisplay = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Focus')
+clock = pygame.time.Clock()
+
+ingame = True
+
+while ingame:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            ingame = False
+
+    start_x, start_y = 100, 100
+    for tile in board.board:
+        pygame.draw.rect(gameDisplay, white, ((start_x + tile.x * 70, start_y + tile.y * 70, 70, 70)), 1 )
+    pygame.display.update()
+    clock.tick(30)
+    
